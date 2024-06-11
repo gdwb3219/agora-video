55f03a2fbe1038aaa38884069e5c652d244b6545
gdwb3219 <gdwb3219@gmail.com>
2024-06-12 00:34:57 +0900

    추후 접속자 Subscribe 기능 테스트 중

--- src/components/VideoCall.jsx
+++ src/components/VideoCall.jsx
@@ -1,5 +1,5 @@
 // src/components/VideoCall.js
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import AgoraRTC from "agora-rtc-sdk-ng";
 import "./VideoCall.css";
 
@@ -13,6 +13,7 @@ function VideoCall({ isOperator }) {
   const client = useRef(null);
   const localContainer = useRef(null);
   const remoteContainer = useRef(null);
+  const [remoteUsers, setRemoteUsers] = useState([]);
 
   useEffect(() => {
     const initAgora = async () => {
@@ -33,34 +34,17 @@ function VideoCall({ isOperator }) {
       }
 
       // 원격 사용자가 참가할 때 처리
-      client.current.on("user-published", async (user, mediaType) => {
-        await client.current.subscribe(user, mediaType);
-        console.log('구독 성공!', user, mediaType)
-        if (mediaType === "video") {
-          const remoteVideoContainer = document.createElement("div");
-          remoteVideoContainer.id = user.uid.toString();
-          remoteVideoContainer.style.width = "400px";
-          remoteVideoContainer.style.height = "300px";
-          remoteContainer.current.appendChild(remoteVideoContainer);
-          user.videoTrack.play(remoteVideoContainer);
-        }
-        if (mediaType === "audio") {
-          user.audioTrack.play();
-        }
-      });
-
+      client.current.on("user-published", handleUserPublished);
       // 원격 사용자가 떠날 때 처리
-      client.current.on("user-unpublished", (user) => {
-        const remoteVideoContainer = document.getElementById(
-          user.uid.toString()
-        );
-        if (remoteVideoContainer) {
-          remoteVideoContainer.remove();
-        }
-      });
+      client.current.on("user-unpublished", handleUserUnpublished);
+      // 
+      client.current.on("user-joined", handleJoined);
+      // 
+      client.current.on("user-left", handleUserLeft);
     };
 
     initAgora();
+    console.log("잘되고 있나 ###############################################")
 
     return () => {
       if (!isOperator) {
@@ -70,13 +54,57 @@ function VideoCall({ isOperator }) {
     };
   }, [isOperator]);
 
+  const handleUserPublished = async (user, mediaType) => {
+    await client.current.subscribe(user, mediaType);
+    console.log('구독 성공!', user, mediaType)
+    if (mediaType === "video") {
+      const remoteVideoContainer = document.createElement("div");
+      remoteVideoContainer.id = user.uid.toString();
+      remoteVideoContainer.style.width = "400px";
+      remoteVideoContainer.style.height = "300px";
+      remoteContainer.current.appendChild(remoteVideoContainer);
+      user.videoTrack.play(remoteVideoContainer);
+    }
+    if (mediaType === "audio") {
+      user.audioTrack.play();
+    }
+
+  }
+
+  const handleUserUnpublished = async (user) => {
+    console.log("User Unpublished", user.uid)
+    const remoteVideoContainer = document.getElementById(
+      user.uid.toString()
+    );
+    if (remoteVideoContainer) {
+      remoteVideoContainer.remove();
+    }
+  }
+
+  const handleJoined = async (user) => {
+    console.log("User Joined:", user.uid);
+    // 원격 사용자가 참가했을 때 해당 사용자를 상태로 관리
+    setRemoteUsers((prevUsers) => [...prevUsers, user]);
+  }
+
+  const handleUserLeft = async (user) => {
+    console.log("User Left", user.uid)
+    setRemoteUsers((prevUsers) => prevUsers.filter(u => u.uid !== user.uid));
+    const remoteVideoContainer = document.getElementById(
+      user.uid.toString()
+    );
+    if (remoteVideoContainer) {
+      remoteVideoContainer.remove();
+    }
+  }
+
   const handleLeave = async () => {
     // console.log(client.current.localTracks, "로컬 트랙이 지금 뭐가 있어?")
     client.current.localTracks.forEach((track) => track.stop());
     // await client.leave();
   }
 
-  console.log("나는 비디오 콜을 실행했었지!!!")
+  console.log("나는 비디오 콜을 실행했었지!!")
 
   return (
     <>
@@ -89,7 +117,12 @@ function VideoCall({ isOperator }) {
           </>
         )}
         <p>Remote User</p>
-        <div ref={remoteContainer} className='remote-container'></div>
+        <div ref={remoteContainer} className="remote-container">
+        {/* Render remote users */}
+        {remoteUsers.map((user) => (
+          <div key={user.uid} id={`user-${user.uid}`} style={{ width: "400px", height: "300px" }}></div>
+        ))}
+      </div>
       </div>
     </>
     

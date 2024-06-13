f9e799db73fba819a1a4b34c607ea59e707a8a47
gdwb3219 <gdwb3219@gmail.com>
2024-06-13 23:05:00 +0900

    Change NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,28 +1,35 @@
-import React from "react";
-import "../css/NavBar.css";
-import { Link } from "react-router-dom";
+import React from 'react';
+import '../css/NavBar.css';
 
 function NavBar() {
   return (
-    <>
-      <div className='navigation'>
-        <nav>
-          <ul>
-            <li>
-              <Link to='/'>Home</Link>
-            </li>
-            <li>
-              <Link to='/about'>About</Link>
-            </li>
-            <li>
-              <Link to='/contact'>Contact</Link>
-            </li>
-          </ul>
-        </nav>
-        <div className="logo">로고</div>
-        <div className="button">버튼</div>
+    <div className="navbar">
+      <div className="nav-header">
+        <button className="nav-button">내 프로필</button>
+
+        <div className="logo-container">
+          <a href="/" className="logo">
+            MonstAR 로고
+          </a>
+        </div>
+
+        <button className="nav-button">매칭 종료하기</button>
       </div>
-    </>
+
+      <div className="cards-container">
+        <div className="card">
+          <span>Step 1. AR 블라인드 대화하기</span>
+        </div>
+        <div className="arrow">▶</div>
+        <div className="card">
+          <span>Step 2. 얼굴 보고 대화하기 (모두 동의 시)</span>
+        </div>
+        <div className="arrow">▶</div>
+        <div className="card">
+          <span>Step 3. 연락처 공유하기</span>
+        </div>
+      </div>
+    </div>
   );
 }
 

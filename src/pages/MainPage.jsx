d636a35023b349153056c52e41de56917c7b1079
DESKTOP-2BPDO9N\gjwot <gjwotjd112@naver.com>
2024-07-12 18:46:09 +0900

    Add Join Access Code

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import tokenData from "../token.json";
 import "../css/App.css";
 
 function MainPage() {
@@ -7,24 +8,49 @@ function MainPage() {
     "MainPage 실행 와드-----------MainPage 실행 와드-----------MainPage 실행 와드-----------MainPage 실행 와드-----------"
   );
   const [isOperator, setIsOperator] = useState(false);
-  const [inCall, setInCall] = useState(false);
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [inputCode, setInputCode] = useState(""); // 사용자 입력 코드 상태
+  const [isCodeValid, setIsCodeValid] = useState(true); // 코드 유효성 상태
+
+  const correctCode = tokenData.entryCode; // token.json에서 입장 코드를 불러옴
+  const navigate = useNavigate(); // useNavigate 훅 사용
+
+  const handleCodeChange = (e) => {
+    setInputCode(e.target.value);
+  };
 
   const joinAsUser = () => {
-    setIsOperator(false);
-    setInCall(true);
+    if (inputCode === correctCode) {
+      setIsOperator(false);
+      setIsCodeValid(true);
+      navigate("/meeting", { state: { isAdmin: false } }); // 페이지 이동
+    } else {
+      setIsCodeValid(false);
+    }
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      joinAsUser();
+    }
   };
 
   return (
     <>
-      <div className='App'>
-        <header className='App-header'>
+      <div className="App">
+        <header className="App-header">
           <h1>Monst AR 랜딩 페이지</h1>
-          <button onClick={joinAsUser}>
-            <Link to='/meeting' state={{ isAdmin: false }}>
-              Join as User
-            </Link>
-          </button>
+          <input
+            type="text"
+            placeholder="Enter code"
+            value={inputCode}
+            onChange={handleCodeChange}
+            onKeyPress={handleKeyPress} // Enter 키 이벤트 핸들러 추가
+            className={isCodeValid ? "" : "invalid-code"}
+          />
+          {!isCodeValid && (
+            <p className="error-message">Invalid code, please try again.</p>
+          )}
+          <button onClick={joinAsUser}>Join as User</button>
         </header>
       </div>
     </>
